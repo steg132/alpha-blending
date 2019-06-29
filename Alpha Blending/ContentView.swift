db78f3c05cf9eaec20d4c036e9983ddd365eb2e0
Ryan Schumacher <steg132@gmail.com>
2019-06-29 19:32:36 -0400

    Initial Commit

---
+++ Alpha Blending/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Alpha Blending
+//
+//  Created by Ryan Schumacher on 6/29/19.
+//  Copyright © 2019 Ryan Schumacher. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
